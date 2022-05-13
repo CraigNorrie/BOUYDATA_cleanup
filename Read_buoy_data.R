ef764b354abd9b457e194cc0cacba36bac03aca7
CraigNorrie <42049924+CraigNorrie@users.noreply.github.com>
2022-05-13 13:31:51 -0700

    Updated to correct wd

--- Read_buoy_data.R
+++ Read_buoy_data.R
@@ -15,9 +15,11 @@ setwd("C:/Users/fooli/OneDrive - UW/Oceanographic data/NOAA national buoy data h
 files46121 <- list.files("C:/Users/fooli/OneDrive - UW/Oceanographic data/NOAA national buoy data historical archives/46121", 
            all.files = FALSE) #Creates a vector with all the filenames in each directory. Need to find a way to automatically give it a filename
 b46121dat <- data.frame()#creates an empty data frame to store data from textfiles
+test <- read.table(file = "46121_2011.txt",header = FALSE, skip=2)
 
 for(i in 1:length(files46121)){ #Need to change this code so that it auctomatically changes for each buoy. Want to put in buoy number only once
-  dat <- read.table(file = files46121[i], fill = TRUE, header = FALSE)
+  dat <- read.table(file = files46121[i], fill = TRUE, header = FALSE, skip=2)
+  dat <- sapply(dat,as.numeric)
   b46121dat <-  bind_rows(b46121dat, dat)
   rm(dat)
 }
