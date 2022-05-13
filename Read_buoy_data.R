ebc47bb5e29a370b2650820bef495ca98723f660
CraigNorrie <42049924+CraigNorrie@users.noreply.github.com>
2022-05-13 13:12:44 -0700

    Initial working to read in bouy data

---
+++ Read_buoy_data.R
@@ -0,0 +1,25 @@
+##Code to read in data from NOAA national buoy database
+
+##Load packages 
+library(tidyverse)
+library(lubridate)
+library(heatwaveR)
+library(Rcpp)
+
+#This code relies on each buoy being in a folder labeled with the bouy number in the main WD
+#AThis code relies on each bouy having its own folder each file should have the naming format "Buoynumber_year.txt"
+# Read in the buoy data ---------------------------------------------------
+
+#REad in the files from th
+setwd("C:/Users/fooli/OneDrive - UW/Oceanographic data/NOAA national buoy data historical archives/46121")
+files46121 <- list.files("C:/Users/fooli/OneDrive - UW/Oceanographic data/NOAA national buoy data historical archives/46121", 
+           all.files = FALSE) #Creates a vector with all the filenames in each directory. Need to find a way to automatically give it a filename
+b46121dat <- data.frame()#creates an empty data frame to store data from textfiles
+
+for(i in 1:length(files46121)){ #Need to change this code so that it auctomatically changes for each buoy. Want to put in buoy number only once
+  dat <- read.table(file = files46121[i], fill = TRUE, header = FALSE)
+  b46121dat <-  bind_rows(b46121dat, dat)
+  rm(dat)
+}
+
+
