42f33197dd7155e458f0e88079559469aa005238
CraigNorrie <42049924+CraigNorrie@users.noreply.github.com>
2022-05-16 15:07:35 -0700

    Updated to read in the data from all years in a folder

--- Read_buoy_data.R
+++ Read_buoy_data.R
@@ -2,26 +2,25 @@
 
 ##Load packages 
 library(tidyverse)
-library(lubridate)
-library(heatwaveR)
-library(Rcpp)
-
+#Save the project folder so that you can return her after importing buoy data
+proj_folder <- getwd()
 #This code relies on each buoy being in a folder labeled with the bouy number in the main WD
-#AThis code relies on each bouy having its own folder each file should have the naming format "Buoynumber_year.txt"
+#AThis code relies on each buoy having its own folder each file should have the naming format "Buoynumber_year.txt"
 # Read in the buoy data ---------------------------------------------------
 
-#REad in the files from th
+#Read in the files from th
 setwd("C:/Users/fooli/OneDrive - UW/Oceanographic data/NOAA national buoy data historical archives/46121")
 files46121 <- list.files("C:/Users/fooli/OneDrive - UW/Oceanographic data/NOAA national buoy data historical archives/46121", 
            all.files = FALSE) #Creates a vector with all the filenames in each directory. Need to find a way to automatically give it a filename
 b46121dat <- data.frame()#creates an empty data frame to store data from textfiles
-test <- read.table(file = "46121_2011.txt",header = FALSE, skip=2)
+
 
 for(i in 1:length(files46121)){ #Need to change this code so that it auctomatically changes for each buoy. Want to put in buoy number only once
-  dat <- read.table(file = files46121[i], fill = TRUE, header = FALSE, skip=2)
-  dat <- sapply(dat,as.numeric)
+  dat <- read.table(file = files46121[i], fill = TRUE, header = TRUE, comment.char = "")#Comment.char is because there are # in the first two lines so they would be skipped if default # was used for a comment
+  dat <- dat[-c(1),]#Remove the second row because this has the units
+  #dat <- sapply(dat,as.numeric)#Makes all data numeric
   b46121dat <-  bind_rows(b46121dat, dat)
   rm(dat)
 }
-
-
+b46121dat[] <- sapply(b46121dat, as.numeric)
+setwd(proj_folder)#Returns wd back to project folder
